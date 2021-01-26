4dd135019673653890fc8ae377d980a85d77c4af
Pavel Polishchuk <pavel_polishchuk@ukr.net>
2021-01-26 10:21:44 +0100

    Remove a srring from pfpp-package.Rd outside the section to avoid warning and intallation error

--- R/functions.R
+++ R/functions.R
@@ -785,3 +785,7 @@ pareto_front <- function(x, y) {
   d$front <- !duplicated(cummin(d$y))
   d[order(d$n), "front"]
 }
+
+
+
+cbpallete <- c("#000000", "#E69F00", "#56B4E9", "#009E73", "#F0E442", "#0072B2", "#D55E00", "#CC79A7")
